ae9ac0a831fc00803fe0ae3d28ebc2f51586cd3b
Michael Robertson <michael_robertson_92@hotmail.co.uk>
2017-06-20 20:45:49 +0100

    added Director cass to complete MVP

--- app/src/main/java/com/example/michaelrobertson/employee/Director.java
+++ app/src/main/java/com/example/michaelrobertson/employee/Director.java
@@ -7,8 +7,13 @@ package com.example.michaelrobertson.employee;
 public class Director extends Manager {
     private double budget;
 
-    public Director(int empId, String name, String ssn, double salary, double budget) {
-        super(empId, name, ssn, salary);
+    public Director(int empId, String name, String ssn, double salary, String deptName, double budget) {
+        super(empId, name, ssn, salary, deptName);
 
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return this.budget;
     }
 }
