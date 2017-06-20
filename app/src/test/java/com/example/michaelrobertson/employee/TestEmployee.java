2158c692aec5c3588b3f721db4a4870d04b94679
Michael Robertson <michael_robertson_92@hotmail.co.uk>
2017-06-20 21:06:34 +0100

    added tests to Employee class

--- app/src/test/java/com/example/michaelrobertson/employee/TestEmployee.java
+++ app/src/test/java/com/example/michaelrobertson/employee/TestEmployee.java
@@ -1,6 +1,41 @@
 package com.example.michaelrobertson.employee;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestEmployee {
 
+Employee employee;
 
+    @Before
+    public void before() {
+        employee = new Employee(1, "John", "ssn", 30000.0 );
+    }
+
+    @Test
+    public void hasEmpId() {
+        assertEquals(1, employee.getEmpId());
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("John", employee.getName());
+    }
+
+    @Test
+    public void hasSsn() {
+        assertEquals("ssn", employee.getSsn());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(30000.0, employee.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        assertEquals(31000.0, employee.raiseSalary(1000), 0.01);
+    }
 }
